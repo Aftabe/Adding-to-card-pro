cbe5721258f1eb791660b3f2b26ddc47b9c80d31
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-04-12 07:23:13 +1000

    string methods used in the js file

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// String method
+const learning = 'Let\'s learn something new today';
+let outPut;
+
+
+
+outPut = learning.length; // To find out the length of a variable
+outPut = learning.concat(' ', ' that we need in the future.'); // To add the values in the string
+outPut = learning.indexOf('l'); // to know the position of the value
+
+
+
+outPut = learning.toLocaleLowerCase();
+outPut = learning.toUpperCase();
+outPut = learning.toLocaleUpperCase();
+
+outPut = learning.substring(0,7);
+outPut = learning.slice(5);
+// outPut = learning.slice(-5);
+
+
+
+
+
+
+
+console.log(outPut);
+
